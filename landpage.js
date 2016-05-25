39b8b6be685473ba5c13224fc5b01890d4e62770
Gabriela Schirmer Mauricio <gabismauricio@gmail.com>
2016-05-25 03:32:50 -0300

    new landing page

---
+++ landpage.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    $('img').click(function(){
+      if ( $(this).attr('id') == 'bonequinho1' ) {
+        $("#bonequinho1").css("opacity", "1");
+        $("#bonequinho2").css("opacity", "0.4");
+        $("#bonequinho3").css("opacity", "0.4");
+        $("#bonequinho4").css("opacity", "0.4");
+        $("#image-background").attr("src", "image-resource/red-background.png");
+        for (var i = 0; i < 7; i++) {
+          $('#'+i).attr('src', 'image-resource/carousel-red-img'+ (i+1) +'.png');
+        }
+      } else {
+        if ( $(this).attr('id') == 'bonequinho2' ) {
+          $("#bonequinho1").css("opacity", "0.4");
+          $("#bonequinho2").css("opacity", "1");
+          $("#bonequinho3").css("opacity", "0.4");
+          $("#bonequinho4").css("opacity", "0.4");
+          $("#image-background").attr("src", "image-resource/qua-background.png");
+          for (var i = 0; i < 7; i++) {
+            $('#'+i).attr('src', 'image-resource/carousel-qua-img'+ (i+1) +'.png');
+          }
+        } else {
+          if ( $(this).attr('id') == 'bonequinho3' ) {
+            $("#bonequinho1").css("opacity", "0.4");
+            $("#bonequinho2").css("opacity", "0.4");
+            $("#bonequinho3").css("opacity", "1");
+            $("#bonequinho4").css("opacity", "0.4");
+            $("#image-background").attr("src", "image-resource/cra-background.png");
+            for (var i = 0; i < 7; i++) {
+              $('#'+i).attr('src', 'image-resource/carousel-cra-img'+ (i+1) +'.png');
+            }
+          } else {
+            if ( $(this).attr('id') == 'bonequinho4' ) {
+              $("#bonequinho1").css("opacity", "0.4");
+              $("#bonequinho2").css("opacity", "0.4");
+              $("#bonequinho3").css("opacity", "0.4");
+              $("#bonequinho4").css("opacity", "1");
+              $("#image-background").attr("src", "image-resource/ova-background.png");
+              for (var i = 0; i < 7; i++) {
+                $('#'+i).attr('src', 'image-resource/carousel-ova-img'+ (i+1) +'.png');
+              }
+            }
+          }
+
+        }
+      }
+    });
+
+
+
+});
+
+location.href = "#";
