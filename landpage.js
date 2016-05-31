c42332f515c7c7df82b10e90c2d47ed52d668829
Gabriela Schirmer Mauricio <gabismauricio@gmail.com>
2016-05-31 17:38:06 -0300

    testando touch 9

--- landpage.js
+++ landpage.js
@@ -46,13 +46,6 @@ $(document).ready(function(){
         }
       }
     });
-
-    $(".carousel-inner").swiperight(function() {
-       $(this).parent().carousel('prev');
-     });
-    $(".carousel-inner").swipeleft(function() {
-       $(this).parent().carousel('next');
-    });
 });
 
 location.href = "#";
